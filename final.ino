2e9e0016ed10d68c78f976f3b9a1863edbe1a85c
Diogo Silva <74576684+diogoaprsilva@users.noreply.github.com>
2020-11-17 02:06:06 +0000

    FInal

---
+++ final.ino
@@ -0,0 +1,240 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <OneWire.h>
+
+
+
+int led1 = D0;
+int led2 = D7;
+int led = D7;
+int photoresistor = A0;
+int power = A5;
+int analogvalue;
+
+int ledstate;
+
+OneWire ds = OneWire(D4);  // 1-wire signal on pin D4
+
+unsigned long lastUpdate = 0;
+
+float lastTemp;
+
+Timer ledTimer(100, readLedState);
+Timer tempTimer(1000, readTemp);
+
+void setup()
+{
+    pinMode(led,OUTPUT);
+    pinMode(photoresistor,INPUT);
+    pinMode(power,OUTPUT);
+    digitalWrite(power,HIGH);
+    Particle.variable("analogvalue", &analogvalue, INT);
+   
+   pinMode(led1, OUTPUT);
+   pinMode(led2, OUTPUT);
+
+   Particle.function("led",ledToggle);
+   
+      Particle.variable("ledstate", &ledstate, INT);
+
+   digitalWrite(led1, LOW);
+   digitalWrite(led2, LOW);
+  Serial.begin(9600);
+  // Set up 'power' pins, comment out if not used!
+  pinMode(D3, OUTPUT);
+  pinMode(D5, OUTPUT);
+  digitalWrite(D3, LOW);
+  digitalWrite(D5, HIGH);
+  ledTimer.start();
+  tempTimer.start();
+}
+
+void loop(){
+analogvalue = analogRead(photoresistor);
+   
+    
+    if(analogvalue>=250){
+        digitalWrite(led, HIGH);
+        Spark.publish("Light_limit","Exceeded",60,PRIVATE);
+        delay(5000); //delay for LED
+        digitalWrite(led, LOW);
+        delay(10000);
+}
+}
+
+void readLedState(){
+    ledstate = digitalRead(led1);
+}
+
+
+void readTemp(void) {
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+
+  if ( !ds.search(addr)) {
+    Serial.println("No more addresses.");
+    Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  // second the CRC is checked, on fail,
+  // print error and just return to try again
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  Serial.println();
+
+  // we have a good address at this point
+  // what kind of chip do we have?
+  // we will set a type_s value for known types or just return
+
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS1820/DS18S20");
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    case 0x26:
+      Serial.println("  Chip = DS2438");
+      type_s = 2;
+      break;
+    default:
+      Serial.println("Unknown device type.");
+      return;
+  }
+
+  // this device has temp so let's read it
+
+  ds.reset();               // first clear the 1-wire bus
+  ds.select(addr);          // now select the device we just found
+  // ds.write(0x44, 1);     // tell it to start a conversion, with parasite power on at the end
+  ds.write(0x44, 0);        // or start conversion in powered mode (bus finishes low)
+
+  // just wait a second while the conversion takes place
+  // different chips have different conversion times, check the specs, 1 sec is worse case + 250ms
+  // you could also communicate with other devices if you like but you would need
+  // to already know their address to select them.
+
+  delay(1000);     // maybe 750ms is enough, maybe not, wait 1 sec for conversion
+
+  // we might do a ds.depower() (parasite) here, but the reset will take care of it.
+
+  // first make sure current values are in the scratch pad
+
+  present = ds.reset();
+  ds.select(addr);
+  ds.write(0xB8,0);         // Recall Memory 0
+  ds.write(0x00,0);         // Recall Memory 0
+
+  // now read the scratch pad
+
+  present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE,0);         // Read Scratchpad
+  if (type_s == 2) {
+    ds.write(0x00,0);       // The DS2438 needs a page# to read
+  }
+
+  // transfer and print the values
+
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s == 2) raw = (data[2] << 8) | data[1];
+  byte cfg = (data[4] & 0x60);
+
+  switch (type_s) {
+    case 1:
+      raw = raw << 3; // 9 bit resolution default
+      if (data[7] == 0x10) {
+        // "count remain" gives full 12 bit resolution
+        raw = (raw & 0xFFF0) + 12 - data[6];
+      }
+      celsius = (float)raw * 0.0625;
+      break;
+    case 0:
+      // at lower res, the low bits are undefined, so let's zero them
+      if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+      if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+      if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+      // default is 12 bit resolution, 750 ms conversion time
+      celsius = (float)raw * 0.0625;
+      break;
+
+    case 2:
+      data[1] = (data[1] >> 3) & 0x1f;
+      if (data[2] > 127) {
+        celsius = (float)data[2] - ((float)data[1] * .03125);
+      }else{
+        celsius = (float)data[2] + ((float)data[1] * .03125);
+      }
+  }
+
+
+  if((((celsius <= 0 && celsius > -1) && lastTemp > 5)) || celsius > 125) {
+      celsius = lastTemp;
+  }
+
+  fahrenheit = celsius * 1.8 + 32.0;
+  lastTemp = celsius;
+  Serial.print("  Temperature = ");
+  Serial.print(celsius);
+  Serial.print(" Celsius, ");
+  Serial.print(fahrenheit);
+  Serial.println(" Fahrenheit");
+
+
+  String temperature = String(celsius); 
+  Particle.publish("temperature", temperature, PRIVATE); 
+}
+int ledToggle(String command) {
+
+
+    if (command=="on") {
+        digitalWrite(led1,HIGH);
+        digitalWrite(led2,HIGH);
+        return 1;
+    }
+    else if (command=="off") {
+        digitalWrite(led1,LOW);
+        digitalWrite(led2,LOW);
+        return 0;
+    }
+    else {
+        return -1;
+    }
+}
